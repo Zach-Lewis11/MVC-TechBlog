3532169eae0b5dc292675d8ae2e178423a1f4625
Zach L <ztak78@gmail.com>
2022-12-21 11:25:59 -0600

    added models, controllers, public js and sever.js

---
+++ public/js/edit.js
@@ -0,0 +1,34 @@
+
+const postId = document.querySelector('input[name="post-id"]').value;
+
+const editPost = async function(event) {
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="post-title"]').value;
+  const body = document.querySelector('textarea[name="post-body"]').value;
+  
+  await fetch(`/api/post/${postId}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+          title,
+          body
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    
+    document.location.replace('/dashboard');
+};
+
+document.querySelector('#edit-post').addEventListener('submit', editPost);
+
+const deletePost = async function() {
+  await fetch(`/api/post/${postId}`, {
+    method: 'DELETE'
+  });
+
+  document.location.replace('/dashboard');
+};
+
+document.querySelector('#delete-btn').addEventListener('click', deletePost);
---
+++ utils/auth.js
@@ -0,0 +1,9 @@
+const hasAuth = (req, res, next) => {
+    if (!req.session.userId) {
+        res.redirect("/login");
+    } else {
+        next();
+    }
+};
+
+module.exports = hasAuth;
